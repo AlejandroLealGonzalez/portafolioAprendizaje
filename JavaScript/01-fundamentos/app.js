db18e3695947bba373bac010856e08d14e34fdcb
lealgalejandro <lealgalejandro@gmail.com>
2025-04-11 12:56:23 -0500

    Agregados fundamentos de JavaScript

---
+++ JavaScript/01-fundamentos/app.js
@@ -0,0 +1,12 @@
+
+let a = 10;
+let b = 20;
+
+let sum = a + b;
+let men = a - b;
+let mult = a * b;
+let div = a / b;
+let mod = a % b;
+let pot = a ** b;
+
+console.log(sum, men, mult, div, mod, pot);
